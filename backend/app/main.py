d44bd096fbe070426ee46e28efa277fd8d0d1343
IzanCoder <74675303+Izan206@users.noreply.github.com>
2026-03-01 13:11:24 +0100

    feat: añadir modelos y conectarlos a la BD en Postgre (#9)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from config import Config
 from db.database import db
+from models.usuario import Usuario
+from models.rutina import Rutina
+from models.ejercicio import Ejercicio
+from models.rutinaejercicio import RutinaEjercicio
 
 app=Flask(__name__)
 
@@ -11,5 +15,10 @@ db.init_app(app)
 def hello_world():
     return "Bienvenido a Invictus!"
 
+with app.app_context():
+    print("creando tablas")
+    db.create_all()
+    print("tablas creadas")
+    
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ backend/app/models/rutinaejercicio.py
@@ -0,0 +1,11 @@
+from db.database import db
+
+class RutinaEjercicio(db.Model):
+    __tablename__="tener"
+    rutina_id=db.Column(db.Integer, db.ForeignKey('rutina.id'), primary_key=True)
+    ejercicio_id=db.Column(db.Integer, db.ForeignKey('ejercicio.id'), primary_key=True)
+    series=db.Column(db.Integer, default=3)
+    repeticiones=db.Column(db.Integer, default=8)
+    
+    rutina = db.relationship("Rutina", back_populates="ejercicios")
+    ejercicio = db.relationship("Ejercicio", back_populates="rutinas")
