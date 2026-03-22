f6aa140b3e29247d9d2facc2639e0344a5f5a4f8
izan.alvarezvarela <alvarezvarelaizan@gmail.com>
2026-03-22 11:20:10 +0100

    feat: añadir endpoint para ver ejercicios de una rutina

--- backend/app/repositories/rutinaejercicio_repo.py
+++ backend/app/repositories/rutinaejercicio_repo.py
@@ -10,6 +10,10 @@ def obtener_rutinaejercicio_por_idrutina_y_idejercicio(id_rutina, id_ejercicio):
     rutina_ejercicio=RutinaEjercicio.query.filter(RutinaEjercicio.rutina_id==id_rutina, RutinaEjercicio.ejercicio_id==id_ejercicio).first()
     return rutina_ejercicio
 
+def obtener_ejercicios_rutina(id_rutina):
+    ejercicios_rutina=RutinaEjercicio.query.filter_by(rutina_id=id_rutina).all()
+    return ejercicios_rutina
+
 def confirmar_eliminacion_ejercicio_de_rutina(rutina_ejercicio):
     db.session.delete(rutina_ejercicio)
     db.session.commit()
