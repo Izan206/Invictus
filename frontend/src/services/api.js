5931fa4086c4d165c9a2f9457eb233a653aab554
izan.alvarezvarela <alvarezvarelaizan@gmail.com>
2026-05-02 19:07:34 +0200

    feat: implementar caducidad JWT

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -18,4 +18,20 @@ api.interceptors.request.use(
   }
 );
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('token_invictus');
+      localStorage.removeItem('email_invictus');
+      localStorage.removeItem('username_invictus');
+      if (window.location.pathname !== '/') {
+        window.location.href = '/';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 export default api;
