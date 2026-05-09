67609372d0c34e39d7f608ecd70776dac82a2fe0
izan.alvarezvarela <alvarezvarelaizan@gmail.com>
2026-05-09 19:35:36 +0200

    feat: añadir funcionalidad crear rutina Front-End

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -35,7 +35,7 @@ def login():
     
     try:
         usuario=autenticacion(email, password_introducida)
-        token_usuario=create_access_token(identity=usuario.email)
+        token_usuario=create_access_token(identity=usuario.username)
         return jsonify({"exito": True, "usuario": usuario.email, "username": usuario.username, "token": token_usuario}), 200
     except CredencialesInvalidasError as e:
         return jsonify({"exito": False, "error": str(e)}), 401
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Inicio from './pages/Inicio';
 import Registro from './pages/Registro';
 import Login from './pages/Login';
 import Catalogo from './pages/Catalogo';
+import TusRutinas from './pages/TusRutinas';
 import Error404 from './pages/Error404';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route element={<MainLayout />}>
         <Route path="/" element={<Inicio />} />
         <Route path="/catalogo" element={<Catalogo />} />
+        <Route path="/rutinas" element={<TusRutinas />} />
         <Route path="*" element={<Error404 />} />
       </Route>
 
